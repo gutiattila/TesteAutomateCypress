e246cfe9c97e505111e574993dbd6a6b3957b5ae
Attila Guti <40992196+gutiattila@users.noreply.github.com>
2023-11-14 22:54:19 +0200

    Add files via upload

--- TesteShopping.cy.js
+++ TesteShopping.cy.js
@@ -4,7 +4,7 @@ describe("Teste shopping",()=>{
     it("testare adaugare produs", ()=>{
 
         cy.visit("https://www.saucedemo.com/")
-         cy.get('[data-test="username"]').type("standard_user");// 
+        cy.get('[data-test="username"]').type("standard_user");// 
         cy.get('[data-test="password"]').type("secret_sauce");
         cy.get('[data-test="login-button"]').click();
         cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
@@ -16,7 +16,7 @@ describe("Teste shopping",()=>{
     it("testare stergere produs", ()=>{
 
         cy.visit("https://www.saucedemo.com/")
-         cy.get('[data-test="username"]').type("standard_user");// 
+        cy.get('[data-test="username"]').type("standard_user");// 
         cy.get('[data-test="password"]').type("secret_sauce");
         cy.get('[data-test="login-button"]').click();
         cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
@@ -31,7 +31,7 @@ describe("Teste shopping",()=>{
     it("testare finalizare cumparare", ()=>{
 
         cy.visit("https://www.saucedemo.com/")
-         cy.get('[data-test="username"]').type("standard_user");// 
+        cy.get('[data-test="username"]').type("standard_user");// 
         cy.get('[data-test="password"]').type("secret_sauce");
         cy.get('[data-test="login-button"]').click();
         cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
--- testeDeLog.cy.js
+++ testeDeLog.cy.js
@@ -12,7 +12,7 @@ describe("Teste de logare",()=>{
     it("test de log in corect",()=>{
 
         cy.visit("https://www.saucedemo.com/")
-         cy.get('[data-test="username"]').type("standard_user");// 
+        cy.get('[data-test="username"]').type("standard_user");// 
         cy.get('[data-test="password"]').type("secret_sauce");
         cy.get('[data-test="login-button"]').click();
         cy.get(".app_logo");
@@ -21,20 +21,20 @@ describe("Teste de logare",()=>{
     it("test de log out", ()=>{
         cy.visit("https://www.saucedemo.com/")
         cy.get('[data-test="username"]').type("standard_user");// 
-       cy.get('[data-test="password"]').type("secret_sauce");
-       cy.get('[data-test="login-button"]').click();
-       cy.get("#react-burger-menu-btn").click();
-       cy.get("#logout_sidebar_link").click();
+        cy.get('[data-test="password"]').type("secret_sauce");
+        cy.get('[data-test="login-button"]').click();
+        cy.get("#react-burger-menu-btn").click();
+        cy.get("#logout_sidebar_link").click();
 
     })
 
     it("test de deschidere inchidere meniu", ()=>{
         cy.visit("https://www.saucedemo.com/")
         cy.get('[data-test="username"]').type("standard_user");// 
-       cy.get('[data-test="password"]').type("secret_sauce");
-       cy.get('[data-test="login-button"]').click();
-       cy.get("#react-burger-menu-btn").click();
-       cy.get("#react-burger-cross-btn").click();
+        cy.get('[data-test="password"]').type("secret_sauce");
+        cy.get('[data-test="login-button"]').click();
+        cy.get("#react-burger-menu-btn").click();
+        cy.get("#react-burger-cross-btn").click();
 
     })
     
